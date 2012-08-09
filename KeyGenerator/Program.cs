8ea0de9b571fd6cb114419e75cbe0f95beeca2ee
Mehrdad Afshari <ma@appharbor.com>
2012-08-08 17:08:41 -0700

    Removed indentation and renamed hmacsha1 to hmacsha256

--- KeyGenerator/Program.cs
+++ KeyGenerator/Program.cs
@@ -1,30 +1,27 @@
 ﻿using System;
 using System.Security.Cryptography;
 
-namespace KeyGenerator
+class Program
 {
-	class Program
+	static void Main(string[] args)
 	{
-		static void Main(string[] args)
+		using (var rijndael = new RijndaelManaged())
 		{
-			using (var rijndael = new RijndaelManaged())
+			rijndael.GenerateKey();
+			rijndael.GenerateIV();
+			using (var hmacsha256 = new HMACSHA256())
 			{
-				rijndael.GenerateKey();
-				rijndael.GenerateIV();
-				using (var hmacsha1 = new HMACSHA256())
-				{
-					hmacsha1.Initialize();
-					Console.WriteLine(template,
-						Convert.ToBase64String(rijndael.Key), 
-						Convert.ToBase64String(rijndael.IV),
-						Convert.ToBase64String(hmacsha1.Key));
-				}
+				hmacsha256.Initialize();
+				Console.WriteLine(template,
+					Convert.ToBase64String(rijndael.Key), 
+					Convert.ToBase64String(rijndael.IV),
+					Convert.ToBase64String(hmacsha256.Key));
 			}
-			Console.ReadKey();
 		}
+		Console.ReadKey();
+	}
 
-		const string template = @"<add key=""cookieauthentication.encryptionkey"" value=""{0}""/>
+	const string template = @"<add key=""cookieauthentication.encryptionkey"" value=""{0}""/>
 <add key=""cookieauthentication.encryptioniv"" value=""{1}""/>
 <add key=""cookieauthentication.validationkey"" value=""{2}""/>";
-	}
 }
